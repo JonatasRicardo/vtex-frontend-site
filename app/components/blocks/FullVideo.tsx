5b4f4864fac6af8c9f0f7125af61a3a9abd58a3e
Jonatas Santos <jonatas.santos@optiisolutions.com>
2024-03-04 11:29:22 -0300

    modal close and bg

--- app/components/blocks/FullVideo.tsx
+++ app/components/blocks/FullVideo.tsx
@@ -4,18 +4,26 @@ import ReactPlayer from "react-player"
 import { Context } from "@/app/vtex/PageContext"
 
 export default function FullVideo() {
-    const { showVideo } = useContext(Context)
+    const { showVideo, setShowVideo } = useContext(Context)
 
     return (
         <>
-            {showVideo && <div className="absolute w-full h-full z-10 t-0">
-                <ReactPlayer
-                    playing
-                    url="https://www.youtube.com/embed/CFvkBMvyxMA"
-                    width="100%"
-                    height="100%"
-                />
-            </div>}
+            {showVideo &&
+                <>
+                    <div className="fixed w-full h-full z-50 t-0 bg-navy bg-opacity-80" onClick={() => setShowVideo(false)} />
+                    <div className="fixed w-11/12 h-[80%] z-50 m-16 t-0">
+                        <div className="text-right pb-3">
+                            <button className="text-white text-2xl" onClick={() => setShowVideo(false)} title="fechar">fechar</button>
+                        </div>
+                        <ReactPlayer
+                            playing
+                            url="https://www.youtube.com/embed/CFvkBMvyxMA"
+                            width="100%"
+                            height="100%"
+                        />
+                    </div>
+                </>
+            }
         </>
     )
 }
